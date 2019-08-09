481aaf78d685a1346ebd73842dbaa7644c618ee4
Yamato Otaka <yamato8010apple@gmail.com>
2019-08-09 13:52:56 +0900

    initial commit

---
+++ ApplicationCoordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplicationCoordinator
+//
+//  Created by 大高倭 on 2019/08/09.
+//  Copyright © 2019 大高倭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
