51073315f17f283c741cab6c0231cf049c1829e9
Yamato Otaka <yamato8010apple@gmail.com>
2019-08-09 13:59:58 +0900

    Create AppCoordinator class

--- ApplicationCoordinator/AppCoordinator.swift
+++ ApplicationCoordinator/AppCoordinator.swift
@@ -6,4 +6,62 @@
 //  Copyright © 2019 大高倭. All rights reserved.
 //
 
-import Foundation
+//import Foundation
+import UIKit
+import UserNotifications
+
+protocol Coordinator {
+    func start<T: Any>(_ type: T)
+}
+
+final class AppCoordinator: Coordinator {
+    private let window: UIWindow
+    private let rootViewController: UIViewController
+
+    enum LaunchType {
+        case normal
+        case notification(_ appState: AppState, userInfo: [AnyHashable: Any], notification: UNNotificationRequest)
+        case userActivity(_ userActivity: NSUserActivity)
+        case openURL(_ url: URL)
+        case shortcutItem(_ shortcutItem: UIApplicationShortcutItem)
+    }
+
+    enum AppState {
+        case inactive
+        case background
+        case active
+        case unknown
+    }
+
+    init(window: UIWindow) {
+        self.window = window
+        self.rootViewController = UIViewController()
+    }
+
+    func start<T>(_ type: T) {
+        guard let type = type as? LaunchType else {
+            return
+        }
+        window.rootViewController = rootViewController
+        let previousVC = window.rootViewController
+        if let previousVC = previousVC {
+            previousVC.dismiss(animated: false) {
+                previousVC.view.removeFromSuperview()
+            }
+        }
+        window.makeKeyAndVisible()
+    }
+
+    static func conversionAppState(_ application: UIApplication) -> AppState {
+        switch application.applicationState {
+        case .active:
+            return .active
+        case .inactive:
+            return .inactive
+        case .background:
+            return .background
+        @unknown default:
+            return .unknown
+        }
+    }
+}
