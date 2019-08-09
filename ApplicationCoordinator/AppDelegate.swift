d3763935da24a809a784a521c9abbeff6f62e792
Yamato Otaka <yamato8010apple@gmail.com>
2019-08-09 14:09:31 +0900

    Modify AppDelegate didFinishLaunchingWithOptions method

--- ApplicationCoordinator/AppDelegate.swift
+++ ApplicationCoordinator/AppDelegate.swift
@@ -17,7 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        appCoordinator = AppCoordinator(window: window)
+        appCoordinator?.start(AppCoordinator.LaunchType.normal)
+
         return true
     }
 
